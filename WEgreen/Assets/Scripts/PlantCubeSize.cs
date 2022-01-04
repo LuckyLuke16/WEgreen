c7094a76acfd14145214f8d7f3e03330570c7941
cmj1231 <willem.kupec@gmail.com>
2022-01-04 16:35:03 +0100

    proper cubes for all models except for mittel and alt Tomaten

--- WEgreen/Assets/Scripts/PlantCubeSize.cs
+++ WEgreen/Assets/Scripts/PlantCubeSize.cs
@@ -17,14 +17,34 @@ public class PlantCubeSize : MonoBehaviour
     void Update()
     {
         //transform.GetComponent<MeshFilter>().mesh.bounds = MeasurePlant.bounds;
-        calculateScales();
-        transform.localScale = new Vector3(xScale, yScale, zScale);
+        if(plant.tag == "aloe")
+        {
+            calculateScalesAloe();
+            transform.localScale = new Vector3(xScale, yScale, zScale);
+            //transform.position = new Vector3(plant.transform.position.x, plant.transform.position.y + 0.08f, plant.transform.position.z);
+            transform.position = new Vector3(plant.transform.position.x, plant.transform.position.y + (MeasurePlant.ySize/2), plant.transform.position.z);
+        }
+        else
+        {
+            calculateScales();
+            transform.localScale = new Vector3(xScale, yScale, zScale);
+            transform.position = new Vector3(plant.transform.position.x, plant.transform.position.y + (MeasurePlant.ySize/2), plant.transform.position.z);
+        }
+    }
+
+
+
+    public void calculateScalesAloe()
+    {
+        xScale = MeasurePlant.xSize + (plant.transform.localScale.x * 100);
+        yScale = MeasurePlant.ySize + ((plant.transform.localScale.y * 100));
+        zScale = MeasurePlant.zSize + (plant.transform.localScale.z * 100);
     }
 
     public void calculateScales()
     {
-        xScale = MeasurePlant.xSize + (plant.transform.localScale.x * 100);
-        yScale = MeasurePlant.ySize + (plant.transform.localScale.y * 100);
-        zScale = MeasurePlant.zSize + (plant.transform.localScale.z * 100);
+        xScale = MeasurePlant.xSize + (plant.transform.localScale.x) - 1;
+        yScale = MeasurePlant.ySize + (plant.transform.localScale.y) - 1;
+        zScale = MeasurePlant.zSize + (plant.transform.localScale.z) - 1;
     }
 }
