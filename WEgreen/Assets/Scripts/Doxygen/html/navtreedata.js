73ddd61b769ae718f16c98a00c4b17b05d049508
Timoj <yabokutv@web.de>
2022-02-01 17:32:19 +0100

    doxygen prepared

--- WEgreen/Assets/Scripts/Doxygen/html/navtreedata.js
+++ WEgreen/Assets/Scripts/Doxygen/html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "Doxygen", "index.html", [
+  [ "WEgreen", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
